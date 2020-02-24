d430fa2adb484c0461d4c9a2cb00abc580a265c1
afidelak <afidelak@gmail.com>
2020-02-24 15:48:38 -0600

    Exercises

---
+++ JS/ex14.js
@@ -0,0 +1,5 @@
+const firstName = "Angela"
+const greeting = "Welcome"
+const endOfMessage = "to learning JavaScript with friends"
+console.log = "%greeting% %firstname% %endOfMessage%"
+console.log = "firstName + greeting + endOfMessage"
