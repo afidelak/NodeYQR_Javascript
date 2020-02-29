2671fddb4bba67551ede94dce7662df570859b2f
afidelak <afidelak@gmail.com>
2020-02-28 19:24:43 -0600

    Exercises

---
+++ JS/ex117.js
@@ -0,0 +1,5 @@
+const students = ["Clinton", "Kumar", "Angie", "Akintude", "Brian"];
+
+
+console.log(students[0], students[2], students[4]);
+
---
+++ JS/ex122.js
@@ -0,0 +1,20 @@
+let ingredients = [
+    "pork", 
+    "tomato", 
+    "chicken", 
+    "lettuce", 
+    "beef", 
+    "carrots", 
+    "cucumber"
+];
+
+let vegetarian = [
+    ingredients[1], 
+    ingredients[3], 
+    ingredients[5], 
+    ingredients[6] 
+]
+    
+
+console.log(ingredients)
+console.log(`The vegetarian ingredients are: ${vegetarian}`);
